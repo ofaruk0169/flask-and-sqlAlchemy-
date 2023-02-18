f41920f710cf6e458b4f7aeeddff983bab0034ce
Omare Faruk <ofaruk0169@gmail.com>
2023-02-18 19:25:22 +0000

    bubble sort and movie titles

--- app.py
+++ app.py
@@ -20,4 +20,26 @@ def movies():
         movies.append({'id' : movie.id, 'title' : movie.title, 'rating' : movie.rating})
     return jsonify({'movies': movies})
 
+@app.route('/sorted_movies')
+def sort_movies():
+    movie_list = Movie.query.all()
+    movies = []
+    for movie in movie_list:
+        movies.append({'id' : movie.id, 'title' : movie.title, 'rating' : movie.rating})
+    movies = sort(movies)
+    return jsonify({'sorted_movies': movies})
+
+def sort(movie_list):
+    print('movie_list is ', movie_list);
+    swap = True
+    i = 0
+    while swap:
+        swap = False
+        for i in range(len(movie_list)-1):
+            next_movie = movie_list[i+1]
+            if movie_list[i]['rating'] < next_movie['rating']:
+                swap = True
+                movie_list[i], movie_list[i+1] = movie_list[i+1], movie_list[i]
+    return movie_list
+
 
